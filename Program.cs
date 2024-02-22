1a4accd533c6d48562cbf61574d19bf8aee5c824
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-02-22 19:25:43 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter two numbers (start and end of the range):");
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        if (start > end)
+        {
+            int temp = start;
+            start = end;
+            end = temp;
+        }
+
+        Console.WriteLine($"Even numbers in the range from {start} to {end}:");
+        for (int i = start; i <= end; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
